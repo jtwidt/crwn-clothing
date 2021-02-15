a3c4fc952ad4e701273a569bc04711060a393794
Jeremy Twidt <jtwidt@gmail.com>
2021-02-15 16:46:14 +0100

    moved directory and shop data to store

---
+++ src/components/collections-overview/CollectionsOverview.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './CollectionsOverview.scss';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollectionsForPreview } from '../../redux/shop/shopSelector';
+import CollectionPreview from '../collection-preview/CollectionPreview';
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className='collections-overview'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import SHOP_DATA from './shopData';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Collection from '../collection/Collection';
 
-const Shop = () => {
-  const [collections, setCollections] = useState(SHOP_DATA);
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
 
+const Shop = ({ match }) => {
   return (
     <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={Collection} />
     </div>
   );
 };
