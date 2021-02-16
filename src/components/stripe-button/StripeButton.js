b3aefeac09a33562cbc7e67c62f6e37a32c4a979
Jeremy Twidt <jtwidt@gmail.com>
2021-02-16 20:50:56 +0100

    updated stripe integration + favicon & title

---
+++ src/components/stripe-button/StripeButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51ILWlBBN0BP05RNb8ecdsJo2plgrrfESfvuBoRUmAIHSWYUUi0gRLbWwjXJr5eJzEBV3rIrqfaVbeCECjGGJxPx000W18sM4Zq';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
