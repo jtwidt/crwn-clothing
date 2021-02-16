0b60ccc7f74868ff957b4c779543425aad57e50b
Jeremy Twidt <jtwidt@gmail.com>
2021-02-16 22:53:59 +0100

    modified sign-in form

--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -59,7 +59,7 @@ const SignIn = () => {
         />
         <div className='buttons'>
           <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn type='button'>
             Sign In With Google
           </CustomButton>
         </div>
